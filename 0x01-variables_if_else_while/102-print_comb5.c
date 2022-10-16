4ed0f097f1602a1b1a3d6ea95e467a6df5a6b2c0
Alle4n <allennajjuma96@gmail.com>
2022-10-16 09:33:14 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints combinations of 2 two-digit numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				for (d = '0'; d <= '9'; d++)
+				{
+					if (!((a == b) && (b == d) && (d == c)))
+					{
+						putchar(48 + a);
+						putchar(48 + x);
+						putchar(' ');
+						putchar(48 + c);
+						putchar(48 + d);
+						putchar(44);
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	Putchar('\n');
+	return (0);
+}
