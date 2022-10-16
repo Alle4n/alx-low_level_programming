74316804e3f8f71c85f9a70499574799cf3d44fa
Alle4n <allennajjuma96@gmail.com>
2022-10-16 09:51:33 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ int main(void)
 			{
 				for (d = 0; d < 10; d++)
 				{
-					if (!((a == b) && (b == d) && (d == c)))
+					if (!((a == b) && (b == c) && (c == d)))
 					{
 						putchar(48 + a);
 						putchar(48 + b);
