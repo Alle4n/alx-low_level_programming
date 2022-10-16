2a615483adb44bb7ba615e869b56aa694b1c9ba8
Alle4n <allennajjuma96@gmail.com>
2022-10-16 09:41:39 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int a, b, c, d;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = 0; a < 10; a++)
 	{
-		for (b = '0'; b <= '9'; b++)
+		for (b = 0; b < 10; b++)
 		{
-			for (c = '0'; c <= '9'; c++)
+			for (c = 0; c < 10; c++)
 			{
-				for (d = '0'; d <= '9'; d++)
+				for (d = 0; d < 10; d++)
 				{
 					if (!((a == b) && (b == d) && (d == c)))
 					{
