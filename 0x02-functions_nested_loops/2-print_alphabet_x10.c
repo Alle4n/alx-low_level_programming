24d961d86b6be03ee1381bc31ba3efd07075a109
Alle4n <allennajjuma96@gmail.com>
2022-10-19 19:57:12 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,10 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char ch;
 
-	while (count <= 9)
+	while (count++ <= 9)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
 		_putchar('\n');
 	}
-	count++;
 }
