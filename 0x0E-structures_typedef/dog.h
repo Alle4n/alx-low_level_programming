9b871447f9201fb1faccad06ddeb71153a426fcc
Alle4n <allennajjuma96@gmail.com>
2023-01-16 17:40:51 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,17 +9,12 @@
  *
  * Description: The attributes of a dog.
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
-
-/**
- *  dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
